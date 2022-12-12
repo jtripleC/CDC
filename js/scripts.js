5c6a9a8f3bbff53e1d0963fb66eb522323ec3eb1
jtripleC <ju4nc4mpos2019@gmail.com>
2022-12-12 00:20:32 -0600

    update

--- js/scripts.js
+++ js/scripts.js
@@ -122,15 +122,15 @@ miCarrito.factory('ProductosService', ['$http', '$filter', function($http, $filt
     });  
 
     
-    var datos = [{"Id": "1", "Categoria": "Librería", "Producto": "Telefono E221 BLANCO", "Precio": "150", "Imagen": "/img/E221 BLANCO.png"},
-    {"Id": "2", "Categoria": "Samsung", "Producto": "Teléfono Galaxy A03 64GB Azul", "Precio": "154.99", "Imagen": "/img/Galaxy A03.png"},
-    {"Id": "3", "Categoria": "Samsung", "Producto": "Teléfono Galaxy A33 128GB Naranja", "Precio": "159", "Imagen": "/img/Galaxy A33.jpeg"},
-    {"Id": "4", "Categoria": "Samsung", "Producto": "Teléfono GALAXY S22 ULTRA SM-S908E", "Precio": "1399", "Imagen": "/img/Galaxy S22 Ultra.png"},
-    {"Id": "5", "Categoria": "Samsung", "Producto": "Teléfono GALAXY Z FOLD 4 SM-F936", "Precio": "1899", "Imagen": "/img/Galaxy Z fold.png"},
-    {"Id": "6", "Categoria": "Redmi", "Producto": "Teléfono Redmi 10A Granite Gray", "Precio": "2.5", "Imagen": "/img/Redmi 10A.jpeg"},
-    {"Id": "7", "Categoria": "Otros", "Producto": "S20FE", "Precio": "150", "Imagen": "/img/S10FE.png"},
-    {"Id": "8", "Categoria": "Samsung", "Producto": "Teléfono Galaxy A03 Core Negro", "Precio": "99", "Imagen": "/img/A03 Core.jpeg"},
-    {"Id": "9", "Categoria": "Otros", "Producto": "Teléfono E30 Gris", "Precio": "139", "Imagen": "/img/E30.png"}];
+    var datos = [{"Id": "1", "Categoria": "Librería", "Producto": "Telefono E221 BLANCO", "Precio": "150", "Imagen": "/img/E221 BLANCO.png", "Descripcion": "Diseño moderno y elegante, para que te sumerjas en el sonido multidimensional de los altavoces estéreo con audio Dolby Atmos®."},
+    {"Id": "2", "Categoria": "Samsung", "Producto": "Teléfono Galaxy A03 64GB Azul", "Precio": "154.99", "Imagen": "/img/Galaxy A03.png", "Descripcion": "Diseño estilizado y fácil de portar,  con pantalla LCD y resolución HD+,  para disfrutar de todos tus contenidos."},
+    {"Id": "3", "Categoria": "Samsung", "Producto": "Teléfono Galaxy A33 128GB Naranja", "Precio": "159", "Imagen": "/img/Galaxy A33.jpeg", "Descripcion": "Diseño delgado y sofisticado, con cámara en la que podrás grabar videos de calidad cinematográfica sin importar si es de día o de noche."},
+    {"Id": "4", "Categoria": "Samsung", "Producto": "Teléfono GALAXY S22 ULTRA SM-S908E", "Precio": "1399", "Imagen": "/img/Galaxy S22 Ultra.png","Descripcion": "Galaxy S22 ULTRA, simetría elegante con su marco pulido y delgado. Con S Pen integrado, expúlsalo desde la parte inferior del smartphone para escribir, hacer un dibujo o controlar tu smartphone. "},
+    {"Id": "5", "Categoria": "Samsung", "Producto": "Teléfono GALAXY Z FOLD 4 SM-F936", "Precio": "1899", "Imagen": "/img/Galaxy Z fold.png", "Descripcion": "Pantalla grande y envolvente con un nuevo diseño Slim.  Galaxy Z Fold son  smartphones en uno con menor peso que uno menos flexible."},
+    {"Id": "6", "Categoria": "Redmi", "Producto": "Teléfono Redmi 10A Granite Gray", "Precio": "2.5", "Imagen": "/img/Redmi 10A.jpeg", "Descripcion": "Diseño liviano y elegante. Con cámara dual que ofrece fotografías increíbles."},
+    {"Id": "7", "Categoria": "Otros", "Producto": "S20FE", "Precio": "150", "Imagen": "/img/S10FE.png", "Descripcion": "Galaxy S20 FE ¡Todo lo que quieras para hacer lo que disfrutas! Pantalla de 6.5 Infinity-O, 6GB de RAM y 128GB ROM de almacenamiento."},
+    {"Id": "8", "Categoria": "Samsung", "Producto": "Teléfono Galaxy A03 Core Negro", "Precio": "99", "Imagen": "/img/A03 Core.jpeg", "Descripcion":"Pantalla HD+ para un experiencia de inmersión y disfrutar de contenido en una gran pantalla."},
+    {"Id": "9", "Categoria": "Otros", "Producto": "Teléfono E30 Gris", "Precio": "139", "Imagen": "/img/E30.png", "Descripcion": "Captura fotos y videos más nítidos con cualquier luz y ve los resultados en la pantalla ultraamplia de 90 Hz."}];
     
     servicio.listar = function(fc){
         setTimeout(function() {
